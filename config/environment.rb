eed83bc3eccd88a33b8e554ce9cae7e0f4ee0851
Maxim <parallel588@gmail.com>
2009-09-09 19:08:12 +0600

    загрузка локалей из всех поддиректорий

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')] 
   config.gem 'authlogic'
   config.gem 'yaroslav-russian', :lib => 'russian', :source => 'http://gems.github.com'
   config.gem "justinfrench-formtastic", :lib     => 'formtastic', :source  => 'http://gems.github.com'
