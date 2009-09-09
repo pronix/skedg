93484752c57e6f2165903ed5b860054d2bbddfe6
Maxim <parallel588@gmail.com>
2009-09-09 23:05:08 +0600

    добавлен метод для локализации в сообщений в контроллере

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,11 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
-
+  private
+  
+  def t_controller(val)
+    t(["controllers", 
+       self.class.to_s.gsub('Controller','').underscore.split('/'),  
+       val].flatten.join('.'))
+  end
 end
