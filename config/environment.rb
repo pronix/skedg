b248bcd53bfe82b00e0a1605bb097f2dadfe5d1e
Maxim <parallel588@gmail.com>
2009-09-09 20:51:09 +0600

    усьановил версию плагина russian не ниже 0.2.1

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')] 
   config.gem 'authlogic'
-  config.gem 'yaroslav-russian', :lib => 'russian', :source => 'http://gems.github.com'
+  config.gem 'yaroslav-russian', :lib => 'russian', :source => 'http://gems.github.com', :version => ">= 0.2.1"
   config.gem "justinfrench-formtastic", :lib     => 'formtastic', :source  => 'http://gems.github.com'
   
   config.time_zone = 'UTC'
