7dcb9b34d815cf8e4a2b65ad6acd5059a808edff
Maxim <parallel588@gmail.com>
2009-09-09 23:06:41 +0600

    реализована авторизация поставщика услуг

--- app/controllers/provider/service_provider_sessions_controller.rb
+++ app/controllers/provider/service_provider_sessions_controller.rb
@@ -5,7 +5,18 @@ class Provider::ServiceProviderSessionsController < Provider::ProvidersControlle
     @provider_session = ServiceProviderSession.new
   end
   def create
+    @provider_session = ServiceProviderSession.new params[:service_provider_session]
+    if @provider_session.save
+      flash[:notice] = t_controller("notice.login")
+      redirect_back_or_default provider_service_providers_path
+    else
+      render :action => :new
+    end   
+    
   end
   def destroy
+    current_provider_session.destroy
+    flash[:notice] =  t_controller("notice.logout")
+    redirect_back_or_default provider_login_path
   end
 end
